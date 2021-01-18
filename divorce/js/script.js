65dd38528aa6122ed7494b9db58c8a7628f8ab0c
kiwiFurry <52002072+kiwiFurry@users.noreply.github.com>
2021-01-19 00:23:49 +0800

    add envidence section

--- divorce/js/script.js
+++ divorce/js/script.js
@@ -1,31 +1,36 @@
 
-    $(document).ready(function(){
-      $(".click-test").click(function(){
-        $(".test1").slideToggle();
-      })
-    });
+$(document).ready(function () {
+  $(".click-test").click(function () {
+    $(".test1").slideToggle();
+  })
+});
 
+$(document).ready(function () {
+  $(".click-sexual-behavior-y2").click(function () {
+    $(".sexaul-behavior2-evidence").slideDown();
 
+  })
+});
+
+$(document).ready(function () {
+  $(".click-sexual-behavior-y1").click(function () {
+    $(".sexaul-behavior2-evidence").slideDown();
+
+  })
+});
+
+$(document).ready(function () {
+  $(".click-sexual-behavior-none").click(function () {
+    $(".sexaul-behavior2-evidence").slideUp();
+
+  })
+});
 
 var times_1 = 0;
 function change1(btn) {
-    btn.style.background = times_1 % 2 == 0 ? 'white' : '#343A40';
-    btn.style.color = times_1 % 2 == 0 ? '#343A40' : 'white';
-    times_1++;
-}
-
-var times_2 = 0;
-function change2(btn) {
-    btn.style.background = times_2 % 2 == 0 ? 'white' : '#343A40';
-    btn.style.color = times_2 % 2 == 0 ? '#343A40' : 'white';
-    times_2++;
-}
-
-var times_3 = 0;
-function change3(btn) {
-    btn.style.background = times_3 % 2 == 0 ? 'white' : '#343A40';
-    btn.style.color = times_3 % 2 == 0 ? '#343A40' : 'white';
-    times_3++;
+  btn.style.background = times_1 % 2 == 0 ? 'white' : '#343A40';
+  btn.style.color = times_1 % 2 == 0 ? '#343A40' : 'white';
+  times_1++;
 }
 
 

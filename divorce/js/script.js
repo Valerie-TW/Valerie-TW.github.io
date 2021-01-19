ea69d3a89b1d1c60e46e2b2e40cca7f20a6fb326
kiwiFurry <52002072+kiwiFurry@users.noreply.github.com>
2021-01-19 22:47:12 +0800

    1/19

--- divorce/js/script.js
+++ divorce/js/script.js
@@ -7,24 +7,56 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $(".click-sexual-behavior-y2").click(function () {
-    $(".sexaul-behavior2-evidence").slideDown();
+    $(".sexaul-behavior2-envidence").slideDown();
 
   })
 });
 
 $(document).ready(function () {
   $(".click-sexual-behavior-y1").click(function () {
-    $(".sexaul-behavior2-evidence").slideDown();
-
+    $(".sexaul-behavior2-envidence").slideDown();   
   })
 });
 
 $(document).ready(function () {
   $(".click-sexual-behavior-none").click(function () {
-    $(".sexaul-behavior2-evidence").slideUp();
-
+    $(".sexaul-behavior2-envidence").slideUp();
+    $("input[name='sexaul-behavior']").prop("checked",false);
+    $("input[name='sexaul-envidence']").prop("checked",false);
   })
 });
+
+$(document).ready(function () {
+  $(".click-sexaul-behavior-children").click(function () {
+    $(".sexaul-behavior2-envidence").slideUp();
+    $("input[name='sexaul-envidence']").prop("checked",false);
+  })
+});
+
+$(document).ready(function () {
+  $(".click-no-envidence").click(function () {
+    $("input[name='sexaul-envidence']").prop("checked",false);
+  })
+});
+
+$(document).ready(function () {
+  $(".click-physical-contact-y").click(function () {
+    $(".physical-contact-envidence").slideDown();   
+  })
+});
+$(document).ready(function () {
+  $(".click-no-phy-envidence").click(function () {
+    $("input[name='physical-contact-envidence']").prop("checked",false);
+  })
+});
+
+$(document).ready(function () {
+  $(".click-physical-contact-n").click(function () {
+    $(".physical-contact-envidence").slideUp();
+    $("input[name='physical-contact-envidence']").prop("checked",false);
+  })
+});
+
 
 var times_1 = 0;
 function change1(btn) {

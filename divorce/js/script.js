a82091dd6a9b074c052e188336a07e376ebdcfa6
kiwiFurry <52002072+kiwiFurry@users.noreply.github.com>
2021-03-14 22:36:10 +0800

    add work experience

--- divorce/js/script.js
+++ divorce/js/script.js
@@ -5,57 +5,57 @@ $(document).ready(function () {
   })
 });
 
-$(document).ready(function () {
-  $(".click-sexual-behavior-y2").click(function () {
-    $(".sexaul-behavior2-envidence").slideDown();
+// $(document).ready(function () {
+//   $(".click-sexual-behavior-y2").click(function () {
+//     $(".sexaul-behavior2-envidence").slideDown();
 
-  })
-});
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-sexual-behavior-y1").click(function () {
-    $(".sexaul-behavior2-envidence").slideDown();   
-  })
-});
+// $(document).ready(function () {
+//   $(".click-sexual-behavior-y1").click(function () {
+//     $(".sexaul-behavior2-envidence").slideDown();   
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-sexual-behavior-none").click(function () {
-    $(".sexaul-behavior2-envidence").slideUp();
-    $("input[name='sexaul-behavior']").prop("checked",false);
-    $("input[name='sexaul-envidence']").prop("checked",false);
-  })
-});
+// $(document).ready(function () {
+//   $(".click-sexual-behavior-none").click(function () {
+//     $(".sexaul-behavior2-envidence").slideUp();
+//     $("input[name='sexaul-behavior']").prop("checked",false);
+//     $("input[name='sexaul-envidence']").prop("checked",false);
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-sexaul-behavior-children").click(function () {
-    $(".sexaul-behavior2-envidence").slideUp();
-    $("input[name='sexaul-envidence']").prop("checked",false);
-  })
-});
+// $(document).ready(function () {
+//   $(".click-sexaul-behavior-children").click(function () {
+//     $(".sexaul-behavior2-envidence").slideUp();
+//     $("input[name='sexaul-envidence']").prop("checked",false);
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-no-envidence").click(function () {
-    $("input[name='sexaul-envidence']").prop("checked",false);
-  })
-});
+// $(document).ready(function () {
+//   $(".click-no-envidence").click(function () {
+//     $("input[name='sexaul-envidence']").prop("checked",false);
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-physical-contact-y").click(function () {
-    $(".physical-contact-envidence").slideDown();   
-  })
-});
-$(document).ready(function () {
-  $(".click-no-phy-envidence").click(function () {
-    $("input[name='physical-contact-envidence']").prop("checked",false);
-  })
-});
+// $(document).ready(function () {
+//   $(".click-physical-contact-y").click(function () {
+//     $(".physical-contact-envidence").slideDown();   
+//   })
+// });
+// $(document).ready(function () {
+//   $(".click-no-phy-envidence").click(function () {
+//     $("input[name='physical-contact-envidence']").prop("checked",false);
+//   })
+// });
 
-$(document).ready(function () {
-  $(".click-physical-contact-n").click(function () {
-    $(".physical-contact-envidence").slideUp();
-    $("input[name='physical-contact-envidence']").prop("checked",false);
-  })
-});
+// $(document).ready(function () {
+//   $(".click-physical-contact-n").click(function () {
+//     $(".physical-contact-envidence").slideUp();
+//     $("input[name='physical-contact-envidence']").prop("checked",false);
+//   })
+// });
 
 
 var times_1 = 0;
@@ -64,6 +64,14 @@ function change1(btn) {
   btn.style.color = times_1 % 2 == 0 ? '#343A40' : 'white';
   times_1++;
 }
+
+// defedndant-salary
+$(document).ready(function () {
+  $(".click-spouse").click(function () {
+    $(".spouse-salary-main").slideUp();
+    console.log("a");
+  })
+});
 
 
 
